7cb8d14e8f37a8a0d818826d30d55e85abe528e5
Pooja Joshi <poojaajoshi94@gmail.com>
2023-03-09 15:36:32 +0530

    Switch Assingment

--- JavaFeb23/src/poojaD/B.java
+++ JavaFeb23/src/poojaD/B.java
@@ -1,7 +1,65 @@
 package poojaD;
 
 public class B {
-	public static void main(String[] args) {
 
-	}
+
+  public static void main (String args[]) {
+	  int monthNumber= 11;
+			  
+	  String month="" ;
+	  
+	   switch (monthNumber){
+		   
+	   case 1:
+		   month="January";
+		   break;
+		   
+	   case	2:
+		   month="February";
+		   break;
+		   
+	   case 3:
+		   month="March";
+		   break;
+		   
+	   case 4:
+		   month="April";
+		   break;
+		   
+	   case 5:
+		   month="May";
+		   break;
+		   
+	   case 6:
+		   month="June";
+		   break;
+		   
+	   case 7:
+		   month="July";
+		   break;
+		   
+	   case 8:
+		   month="August";
+		   break;
+		   
+	   case 9:
+		   month="September";
+		   break;
+	   case 10:
+		    month="October"; 
+		    break;
+		    
+	   case 11:
+		   month="November";
+		   break;
+	   case 12:
+		   month="December";
+		   break;
+		   default:
+			   month="Invalid Input";
+		   
+	  
+  }
+	   System.out.println(month);  
+  }
 }
